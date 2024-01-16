bb019b1f86248aa34c58e0b3eb3f1a4f2848364d
Vitaliy <83842891+Odds112@users.noreply.github.com>
2024-01-17 02:35:40 +0500

    com_sl

---
+++ sl_main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import compare as comp
+
+
+def main():
+    st.title("Пример приложения Streamlit")
+
+    # Окна для ввода текста
+    text_input1 = st.text_area("Введите текст 1:", "")
+    text_input2 = st.text_area("Введите текст 2:", "")
+
+    # Кнопка для запуска
+    if st.button("Сравнить тексты"):
+        # Вызываем функцию для сравнения текстов
+        result = comp.compare_texts(text_input1, text_input2)
+
+        # Выводим результат
+        st.write("Результат сравнения текстов:")
+        st.write(result)
+
+if __name__ == "__main__":
+    main()
